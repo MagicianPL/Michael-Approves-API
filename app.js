b62f30f1f42deadd865925fd8eea3091dbe839c3
Michał <sukowskimichal@gmail.com>
2021-12-12 10:08:02 +0100

    dotenv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv/config");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const restaurantsRouter = require("./routes/restaurants");
